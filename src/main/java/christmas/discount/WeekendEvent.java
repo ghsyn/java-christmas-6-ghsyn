1626070283c4505e62b7faa96d1628e8e258ac11
ghsyn <swanshawn715@gmail.com>
2023-11-12 22:27:17 +0900

    Feat: create a WeekendEvent class that implements Discount

---
+++ src/main/java/christmas/discount/WeekendEvent.java
@@ -0,0 +1,25 @@
+package christmas.discount;
+
+public class WeekendEvent implements Discount {
+    private int salePrice;
+    private static final int MAIN_SALE_AMOUNT = 2023;
+
+    public void setSalePrice(String[][] mainMenuArray) {
+        int numberOfMain = 0;
+        try {
+            for (String count : mainMenuArray[0]) {
+                numberOfMain += Integer.parseInt(count);
+            }
+        } catch (NumberFormatException e) {
+            // TODO: IllegalArgumentException 처리
+            throw new IllegalArgumentException();
+        }
+
+        salePrice = MAIN_SALE_AMOUNT * numberOfMain;
+    }
+
+    @Override
+    public int doSale(int originalPrice) {
+        return originalPrice - salePrice;
+    }
+}
