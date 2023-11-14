12a51d6090341f10f908905a6e2974628f4c9fa4
ghsyn <swanshawn715@gmail.com>
2023-11-14 17:30:10 +0900

    Feat: create a Calendar class
    - The calendar class consists of the getDayIndex method and the findDayOfWeek method.
    - getDayIndex(): Create a calendar based on the start day of the week.
    - findDayOfWeek(): If the visit date is received as a parameter, it finds what day of the week it is.

---
+++ src/main/java/christmas/Calendar.java
@@ -0,0 +1,22 @@
+package christmas;
+
+public class Calendar {
+    private static final String[] DAYS_OF_THE_WEEK = {"월", "화", "수", "목", "금", "토", "일"};
+    private static final String START_DAY_OF_DECEMBER = "금";
+    private static int dayIndex;
+
+    private static void getDayIndex() {
+        for (int i = 0; i < DAYS_OF_THE_WEEK.length; i++) {
+            if (DAYS_OF_THE_WEEK[i].equals(START_DAY_OF_DECEMBER)) {
+                dayIndex = i;
+            }
+        }
+    }
+
+    public static String findDayOfWeek(int visitDate) {
+        getDayIndex();
+        String dayOfVisitDate = DAYS_OF_THE_WEEK[visitDate % 7 + dayIndex - 1];
+
+        return dayOfVisitDate;
+    }
+}
