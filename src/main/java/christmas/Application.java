46635825fe99399e1d01b59b00da3cde8be1be50
ghsyn <swanshawn715@gmail.com>
2023-11-15 17:30:57 +0900

    Feat: complete the Application and Planner class

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,20 @@
 package christmas;
 
+import christmas.view.*;
+
 public class Application {
+    public static int VISIT_DATE;
+    private static String[][] ORDER;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView.startMessage();
+
+        VISIT_DATE = InputView.readDate();
+        ORDER = InputView.readOrder();
+
+        OutputView.printPreviewMessage(VISIT_DATE);
+
+        Planner planner = new Planner(VISIT_DATE, ORDER);
+        planner.createEventPlanner(ORDER);
     }
 }
--- src/main/java/christmas/Planner.java
+++ src/main/java/christmas/Planner.java
@@ -1,7 +1,102 @@
 package christmas;
 
+import christmas.badge.*;
+import christmas.controller.*;
+import christmas.discount.*;
+import christmas.domain.*;
+import christmas.domain.menu.*;
+import christmas.exception.IllegalArgumentExceptionHandler;
+import christmas.giveaway.*;
+import christmas.view.*;
+
+import java.util.List;
+
 public class Planner {
-    // 예제
-    private static final int VISIT_DATE = 1;
-    private static String[][] order = {{"양송이수프"}, {"2"}};
+    private final List<Discount> discountEvents = List.of(new ChristmasEvent(), new WeekdayEvent(), new WeekendEvent(), new SpecialEvent());
+    private final List<Giveaway> giveawayEvents = List.of(new ChampagneEvent());
+    private final List<BadgeGrant> badgeGrantEvents = List.of(new DecemberEvent());
+    private int originalPrice;
+    private final String visitDay;
+    private int salePrice;
+    private int giftPrice;
+
+    public Planner(int visitDate, String[][] order) {
+        this.visitDay = Calendar.findDayOfWeek(visitDate);
+        this.originalPrice = getOriginalPrice(order);
+        salePrice = 0;
+        giftPrice = 0;
+    }
+
+    private int getOriginalPrice(String[][] order) {
+        for (String[] item : order) {
+            String korName = item[0];
+            String quantity = item[1];
+
+            originalPrice += getItemPrice(korName, quantity);
+        }
+        return originalPrice;
+    }
+
+    private int getItemPrice(String korName, String quantity) {
+        try {
+            for (Menu menu : Menu.getAllItems()) {
+                if (menu.getKorName().equals(korName)) {
+                    return menu.getPrice() * Integer.parseInt(quantity);
+                }
+            }
+            throw new IllegalArgumentException(korName + "은(는) 없는 메뉴입니다.");
+        } catch (NumberFormatException e) {
+            new IllegalArgumentExceptionHandler("유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        } catch (IllegalArgumentException e) {
+            new IllegalArgumentExceptionHandler(e.getMessage());
+        }
+        return 0;
+    }
+
+    private void applyEvents(String[][] order) {
+        if (originalPrice < 10000) {
+            return;
+        }
+        for (Discount discountEvent : discountEvents) {
+            DiscountValidator.doDiscount(discountEvent, visitDay, order);
+            salePrice += discountEvent.getSalePrice();
+        }
+        for (Giveaway giveawayEvent : giveawayEvents) {
+            GiveawayValidator.comparePrice(giveawayEvent, originalPrice);
+            if (giveawayEvent.getIsApplied()) {
+                giftPrice += giveawayEvent.getMenuPrice();
+            }
+        }
+        for (BadgeGrant badgeGrantEvent : badgeGrantEvents) {
+            BadgeGrantValidator.comparePrice(badgeGrantEvent, salePrice + giftPrice);
+        }
+    }
+
+    public void createEventPlanner(String[][] order) {
+        OutputView.printOrder(order);
+        OutputView.printOriginalPrice(originalPrice);
+
+        applyEvents(order);
+
+        OutputView.printGiveawayMessage();
+        if (giftPrice != 0) {
+            giveawayEvents.forEach(OutputView::printGiveaway);
+        } else {
+            OutputView.nothingMessage();
+        }
+
+        OutputView.printBenefitListMessage();
+        if ((salePrice + giftPrice) != 0) {
+            discountEvents.forEach(OutputView::printDiscountBenefitList);
+            giveawayEvents.forEach(OutputView::printGiveawayBenefitList);
+        } else {
+            OutputView.nothingMessage();
+        }
+
+        OutputView.printTotalBenefitPrice(salePrice + giftPrice);
+        OutputView.printPayPrice(originalPrice + salePrice);
+        for (BadgeGrant badgeGrantEvent : badgeGrantEvents) {
+            OutputView.printBadge(badgeGrantEvent);
+        }
+    }
 }
