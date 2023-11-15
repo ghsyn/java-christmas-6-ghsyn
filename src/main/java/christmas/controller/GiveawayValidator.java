7f169ce71fc170aca0f54c84fd9a95037b26a1c1
ghsyn <swanshawn715@gmail.com>
2023-11-15 17:26:51 +0900

    Refactor: refactor comparePrice method

--- src/main/java/christmas/controller/GiveawayValidator.java
+++ src/main/java/christmas/controller/GiveawayValidator.java
@@ -5,7 +5,7 @@ import christmas.giveaway.*;
 public class GiveawayValidator {
     private static final int CHAMPAGNE_EVENT_MINIMUM_ORIGINAL_PRICE = 120000;
     public static void comparePrice(Giveaway giveawayEvent, int originalPrice) {
-        if (originalPrice > CHAMPAGNE_EVENT_MINIMUM_ORIGINAL_PRICE) {
+        if (originalPrice >= CHAMPAGNE_EVENT_MINIMUM_ORIGINAL_PRICE) {
             giveawayEvent.doGiveaway(originalPrice);
         }
     }
